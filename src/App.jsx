c2513470976f32c942800eeb447637e350fa9209
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-07-08 20:11:50 -0400

    feat: fetch and display 100 pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,38 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [names, setNames] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(`${import.meta.env.VITE_BASE_URL}pokemon?limit=100000`)
-      const json = await res.json();
-      setNames(json);
-      console.log(json);
-    }
-    fetchData();
-  }, []);
+    async function fetchPokemons() {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+      const data = await res.json();
 
+      const pokemonsWithImages = data.results.map((pokemon) => {
+        const id = pokemon.url.split('/').filter(Boolean).pop();
+        const image = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${id}.svg`;
+        return { name: pokemon.name, image };
+      });
+
+      setPokemonList(pokemonsWithImages);
+    }
+
+    fetchPokemons();
+  }, []);
+  
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h1>Pokémon</h1>
+      <ul>
+        {pokemonList.map((pokemon) => (
+          <li key={pokemon.name}>
+            <img src={pokemon.image} alt={pokemon.name}/>
+            <p>{pokemon.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App
